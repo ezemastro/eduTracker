ccf9a8f073fe9028438b3fe286691d6eb33a57f1
ezemastro <mastropietroezequiel@gmail.com>
2026-03-06 20:32:33 +0000

    Set outputDir to server in Astro

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -8,8 +8,9 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
+  output: "server",
 
-  integrations: [react()]
+  integrations: [react()],
 });
