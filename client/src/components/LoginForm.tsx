b3d6a0ae54f0139978a1e2186cdb7706e22a5a30
ezemastro <mastropietroezequiel@gmail.com>
2026-03-08 13:43:56 +0000

    Add error handling and feedback to login form

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -2,17 +2,26 @@ import { Form } from "@/components/ui/form";
 import { Field, FieldError, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { navigate } from "astro:transitions/client";
 
 export default function LoginForm() {
+  const [isError, setIsError] = useState(false);
   const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
-    await fetch("/api/login", {
+    const res = await fetch("/api/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ password: e.currentTarget.password.value }),
     });
+    if (res.status === 200) {
+      setIsError(false);
+      navigate("/");
+    } else {
+      setIsError(true);
+    }
   };
   return (
     <Form onSubmit={handleSubmit}>
@@ -26,6 +35,11 @@ export default function LoginForm() {
         />
         <FieldError>La contraseña es requerida</FieldError>
       </Field>
+      {isError && (
+        <p className="text-destructive-foreground text-xs">
+          Contraseña incorrecta
+        </p>
+      )}
       <Button type="submit">Acceder</Button>
     </Form>
   );
