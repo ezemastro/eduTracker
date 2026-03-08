9280de11eba59bc69863c391402a60079487547a
ezemastro <mastropietroezequiel@gmail.com>
2026-03-08 15:40:51 +0000

    Add session management and token verification in middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -3,18 +3,18 @@ import { verifyProtectedRoute } from "./lib/protectedRoutes";
 import { verifyToken } from "./lib/token";
 
 export const onRequest = defineMiddleware(async (context, next) => {
-  const { url, cookies, redirect } = context;
-  const isProtectedRoute = verifyProtectedRoute(url.pathname);
-  if (!isProtectedRoute) {
-    return next();
-  }
+  const { url, cookies, redirect, locals } = context;
 
+  // Verify token
   const token = cookies.get("auth_token")?.value;
-  if (!token) {
-    return redirect("/login");
-  }
-  const isValidToken = await verifyToken(token);
-  if (!isValidToken) {
+  const isValidToken = !!(token && (await verifyToken(token)));
+
+  // Save session boolean
+  locals.isLoggedIn = isValidToken;
+
+  // Redirect to login if trying to access a protected route without a valid token
+  const isProtectedRoute = verifyProtectedRoute(url.pathname);
+  if (isProtectedRoute && !isValidToken) {
     return redirect("/login");
   }
   return next();
