fd71b1f87df699dfddb6e2812b89f1d9477bd956
ezemastro <mastropietroezequiel@gmail.com>
2026-03-12 18:21:17 +0000

    Refactor setInitialData to dynamically import initial data

--- client/src/utils/setInitialData.ts
+++ client/src/utils/setInitialData.ts
@@ -1,20 +1,25 @@
 import { db } from "@/lib/database";
-import { initialData } from "data/intialData";
 
 export const setInitialData = async () => {
-  Object.entries(initialData).forEach(([groupName, students]) => {
-    const year = parseInt(groupName[0]);
-    const letter = groupName[1];
-    db.groups.create(year, letter).then((id) => {
-      students.forEach((student) => {
-        db.students.create({
-          name: student.name,
-          lastName: student.lastName,
-          gender: student.gender,
-          image: student.image,
-          group_id: id,
+  try {
+    const { initialData } = await import("../../data/intialData");
+
+    Object.entries(initialData).forEach(([groupName, students]) => {
+      const year = parseInt(groupName[0]);
+      const letter = groupName[1];
+      db.groups.create(year, letter).then((id) => {
+        students.forEach((student) => {
+          db.students.create({
+            name: student.name,
+            lastName: student.lastName,
+            gender: student.gender,
+            image: student.image,
+            group_id: id,
+          });
         });
       });
     });
-  });
+  } catch (error) {
+    console.warn("No initial data in data/initialData.ts", error);
+  }
 };
